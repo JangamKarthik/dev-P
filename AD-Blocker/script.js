90de28ddffeef27f5e3e651f76bef03f6d7a33eb
Karthik_Jangam <102133551+JangamKarthik@users.noreply.github.com>
2024-01-18 17:36:43 +0530

    Add files via upload

---
+++ AD-Blocker/script.js
@@ -0,0 +1,6 @@
+setInterval(function(){
+    var skipButton = document.getElementsByClassName("ytp-ad-skip-button");
+    if(skipButton != undefined && skipButton.length>0){
+        skipButton[0].click();
+    }
+},100)
